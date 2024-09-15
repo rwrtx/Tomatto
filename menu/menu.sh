77abaf0ad3a36741b484b0fc14db1bddf56ab8a7
rwrtx <152061298+rwrtx@users.noreply.github.com>
2024-09-16 00:09:01 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -361,7 +361,7 @@ DATE=$(date +'%Y-%m-%d')
 datediff() {
 d1=$(date -d "$1" +%s)
 d2=$(date -d "$2" +%s)
-#echo -e "$COLOR1 $NC Expiry In   : $(( (d1 - d2) / 86400 )) Days"
+echo -e "$COLOR1 $NC Expiry In   : $(( (d1 - d2) / 86400 )) Days"
 }
 function new(){
 cat> /etc/cron.d/autocpu << END
